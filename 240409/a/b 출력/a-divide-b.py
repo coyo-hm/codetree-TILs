ef0e40e5baeacfd5759167adc9b3e0a6c24eb994
COYO <56423604+coyo-hm@users.noreply.github.com>
2024-04-09 16:04:01 +0900

    Title: a/b 출력, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240409/a/b 출력/a-divide-b.py
+++ 240409/a/b 출력/a-divide-b.py
@@ -1,7 +1,8 @@
 import math
 a, b = map(int, input().split())
-temp = 1e20
-answer = str(a // b) + "."
-a = a % b
-answer += str(math.floor(a / b * temp))
-print(answer)
+print(f"{a//b}.", end="")
+a %= b
+for _ in range(20):
+    a *= 10
+    print(a // b, end="")
+    a %= b
