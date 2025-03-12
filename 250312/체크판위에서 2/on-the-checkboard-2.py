bafa7d9d1f884bc43c831920b8f7fe9f4f3ec42a
COYO <56423604+coyo-hm@users.noreply.github.com>
2025-03-12 19:07:11 +0900

    Title: 체크판위에서 2, Time: 51ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250312/체크판위에서 2/on-the-checkboard-2.py
+++ 250312/체크판위에서 2/on-the-checkboard-2.py
@@ -4,11 +4,10 @@ grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
 ans = 0
-if grid[R - 1][C - 1] != grid[0][0]:
-    for i in range(R - 2):
-        for j in range(C - 2):
-            for k in range(i + 1, R - 1):
-                for l in range(j + 1, C - 1):
-                    if grid[0][0] == grid[k][l] and grid[R - 1][C - 1] == grid[i][j]:
-                        ans += 1    
+for i in range(R - 2):
+    for j in range(C - 2):
+        for k in range(i + 1, R - 1):
+            for l in range(j + 1, C - 1):
+                if grid[0][0] == grid[k][l] and grid[R - 1][C - 1] == grid[i][j] and grid[i][j] != grid[0][0]:
+                    ans += 1    
 print(ans)
