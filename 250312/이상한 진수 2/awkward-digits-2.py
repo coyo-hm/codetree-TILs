8c963faf05318128df19aecba5b8a7964636fc55
COYO <56423604+coyo-hm@users.noreply.github.com>
2025-03-12 20:47:43 +0900

    Title: 이상한 진수 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250312/이상한 진수 2/awkward-digits-2.py
+++ 250312/이상한 진수 2/awkward-digits-2.py
@@ -1,8 +1,8 @@
 a = input()
 
 # Please write your code here.
-ans = int(a, 2)
+N = 0
 for i in range(1, len(a)):
     num = a[:i] + ("1" if a[i] == "0" else "0") + a[i + 1:]
-    ans = max(int(num, 2), ans)
-print(ans)
+    N = max(int(num, 2), N)
+print(N)
