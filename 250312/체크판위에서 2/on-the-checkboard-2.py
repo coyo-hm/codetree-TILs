41e02f2925af801b2a88550449c446dcc1e4f98e
COYO <56423604+coyo-hm@users.noreply.github.com>
2025-03-12 19:10:02 +0900

    Title: 체크판위에서 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250312/체크판위에서 2/on-the-checkboard-2.py
+++ 250312/체크판위에서 2/on-the-checkboard-2.py
@@ -4,8 +4,8 @@ grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
 ans = 0
-for i in range(R - 2):
-    for j in range(C - 2):
+for i in range(1, R - 2):
+    for j in range(1, C - 2):
         for k in range(i + 1, R - 1):
             for l in range(j + 1, C - 1):
                 if grid[0][0] == grid[k][l] and grid[R - 1][C - 1] == grid[i][j] and grid[i][j] != grid[0][0]:
