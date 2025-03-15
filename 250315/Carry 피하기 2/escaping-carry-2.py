ae9e0c37fa47124acbc55bc0605e145391bee39c
COYO <56423604+coyo-hm@users.noreply.github.com>
2025-03-15 17:48:55 +0900

    Title: Carry 피하기 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250315/Carry 피하기 2/escaping-carry-2.py
+++ 250315/Carry 피하기 2/escaping-carry-2.py
@@ -7,9 +7,9 @@ answer = -1
 def is_no_carry(a, b, c):
     if a == b == c == 0:
         return True
-    if (a % 10) + (b % 10) + (c % 10) > 10:
+    if (a % 10) + (b % 10) + (c % 10) >= 10:
         return False
-    return is_no_carry (a // 10, b // 10, c // 10)
+    return is_no_carry(a // 10, b // 10, c // 10)
 
 for i in range(n):
     for j in range(i + 1, n):
